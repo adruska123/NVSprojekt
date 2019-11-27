79d49bbfed9005e3ed425acb8d1f772a41406880
Adam Druska <a_druska@utb.cz>
2019-11-27 20:57:04 +0100

    Create random number

--- A1NVS/Program.cs
+++ A1NVS/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace A1NVS
 {
     class Program
     {
+        public static List<int> Numbers = new List<int>();
+        
         static void Main(string[] args)
         {
-           
+           if (args.Length == 0)
+            {
+                RandomNumber rn = new RandomNumber();
+                for(int i = 0; i < 20; i++)
+                {
+                    Numbers.Add(rn.RandomInt(0, 100));                     
+                }
+            }             
         }
     }
 }
